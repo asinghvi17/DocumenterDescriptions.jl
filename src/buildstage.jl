60489896fed142b2f830ca851c9061442dd69f51
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-03-01 14:30:04 -0500

    better description hijack, try-catch

--- src/buildstage.jl
+++ src/buildstage.jl
@@ -10,8 +10,20 @@ function Documenter.Selectors.runner(::Type{DescriptionAdder}, doc::Documenter.D
     for (filename, page) in doc.blueprint.pages
         # If there is a description then we shouldn't do anything,
         # otherwise we should insert a description.
-        get!(page.globals.meta, :Description) do 
-            plugin(page, doc)
+        if haskey(page.globals.meta, :Description)
+            @debug "DocumenterDescriptions: Page at $filename already has a description, skipping"
+        else 
+            @debug "DocumenterDescriptions: Page at $filename has no description, generating one"
+            # Call the plugin on the page
+            new_description = plugin(page, doc)
+            # If the plugin returns `nothing`, it failed to add a description
+            # Note that this is different from the plugin erroring, or returning an empty string
+            # We catch this state and warn here.
+            if isnothing(new_description)
+                @warn "DocumenterDescriptions: Failed to generate description for page at $filename"
+            else
+                page.globals.meta[:Description] = new_description
+            end
         end
     end
 end
